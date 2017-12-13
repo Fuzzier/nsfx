e4562148988347359a09dd4f33d30699b5880bd5
Wei Tang <gauchyler@uestc.edu.cn>
2017-12-13 23:00:08 +0800

    Added "chrono.h" at project root directory.

---
+++ chrono.h
@@ -0,0 +1,29 @@
+﻿/**
+ * @file
+ *
+ * @brief Chrono support for Network Simulation Frameworks.
+ *
+ * @version 1.0
+ * @author  Wei Tang <gauchyler@uestc.edu.cn>
+ * @date    2017-12-13
+ *
+ * @copyright Copyright (c) 2017.
+ *   National Key Laboratory of Science and Technology on Communications,
+ *   University of Electronic Science and Technology of China.
+ *   All rights reserved.
+ */
+
+
+
+#ifndef CHRONO_H__6C1D0D2F_9CA5_4BEB_A1EC_9C01534A3569
+#define CHRONO_H__6C1D0D2F_9CA5_4BEB_A1EC_9C01534A3569
+
+
+#include <nsfx/chrono/config.h>
+#include <nsfx/chrono/duration.h>
+#include <nsfx/chrono/time-point-concept.h>
+#include <nsfx/chrono/virtual-time-point.h>
+
+
+#endif // CHRONO_H__6C1D0D2F_9CA5_4BEB_A1EC_9C01534A3569
+
