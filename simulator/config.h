be9cca7a5b6ade6536d746ccb2bd73cd1d69439e
Fuzzier Tang <gauchyler@uestc.edu.cn>
2017-12-27 16:52:47 +0800

    Removed OPNET adapters.

--- simulator/config.h
+++ simulator/config.h
@@ -18,19 +18,30 @@
 
 
 #include <nsfx/config.h>
-#include <nsfx/chrono/duration.h>
-#include <nsfx/chrono/virtual-time-point.h>
 
 
 /**
  * @defgroup Simulator
  * @brief The network simulator.
+ *
+ * Users can define their own \c Duration and \c TimePoint types in the global
+ * namespace or in the \c nsfx namespace, and define macro
+ * \c NSFX_SIMULATOR_CHRONO_DEFINED before including simulator headers.<br/>
+ * The simulator library will use the user-defined types.
  */
 
 
 ////////////////////////////////////////////////////////////////////////////////
-typedef chrono::Duration          Duration;
-typedef chrono::VirtualTimePoint  TimePoint;
+// Chrono definitions.
+#if !defined(NSFX_SIMULATOR_CHRONO_DEFINED)
+# include <nsfx/chrono/duration.h>
+# include <nsfx/chrono/virtual-time-point.h>
+# define NSFX_SIMULATOR_CHRONO_DEFINED
+  typedef chrono::Duration          Duration;
+  typedef chrono::VirtualTimePoint  TimePoint;
+#else // if defined(NSFX_SIMULATOR_CHRONO_DEFINED)
+BOOST_CONCEPT_ASSERT((TimePointConcept<TimePoint>));
+#endif // !defined(NSFX_SIMULATOR_CHRONO_DEFINED)
 
 
 #endif // CONFIG_H__CC642C06_21DD_48A9_92CD_4C753FCDE2C5
