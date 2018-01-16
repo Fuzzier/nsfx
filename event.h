a65dc9e72db1160220708030e0c0ea186d86f5f6
Fuzzier Tang <gauchyler@gmail.com>
2018-01-16 11:40:45 +0800

    Added header files for each module.

---
+++ event.h
@@ -0,0 +1,31 @@
+﻿/**
+ * @file
+ *
+ * @brief Event support for Network Simulation Frameworks.
+ *
+ * @version 1.0
+ * @author  Fuzzier Tang <gauchyler@gmail.com>
+ * @date    2018-01-16
+ *
+ * @copyright Copyright (c) 2018.
+ *            National Key Laboratory of Science and Technology on Communications,
+ *            University of Electronic Science and Technology of China.
+ *            All rights reserved.
+ */
+
+#ifndef EVENT_H__A144D68C_F756_4043_8212_1E563AE1D32B
+#define EVENT_H__A144D68C_F756_4043_8212_1E563AE1D32B
+
+
+#include <nsfx/event/config.h>
+#include <nsfx/event/exception.h>
+
+#include <nsfx/event/i-event-sink.h>
+#include <nsfx/event/event-sink.h>
+
+#include <nsfx/event/i-event.h>
+#include <nsfx/event/event.h>
+
+
+#endif // EVENT_H__A144D68C_F756_4043_8212_1E563AE1D32B
+
---
+++ simulation.h
@@ -0,0 +1,36 @@
+﻿/**
+ * @file
+ *
+ * @brief Simulation support for Network Simulation Frameworks.
+ *
+ * @version 1.0
+ * @author  Fuzzier Tang <gauchyler@gmail.com>
+ * @date    2018-01-16
+ *
+ * @copyright Copyright (c) 2018.
+ *            National Key Laboratory of Science and Technology on Communications,
+ *            University of Electronic Science and Technology of China.
+ *            All rights reserved.
+ */
+
+#ifndef SIMULATION_H__3558CCE7_4DE5_4B7F_A9CF_44591D238478
+#define SIMULATION_H__3558CCE7_4DE5_4B7F_A9CF_44591D238478
+
+
+#include <nsfx/simulation/config.h>
+#include <nsfx/simulation/exception.h>
+
+#include <nsfx/simulation/i-clock.h>
+#include <nsfx/simulation/i-event-handle.h>
+#include <nsfx/simulation/i-event-scheduler.h>
+#include <nsfx/simulation/list-event-scheduler.h>
+
+#include <nsfx/simulation/i-simulator.h>
+#include <nsfx/simulation/simulator.h>
+
+#include <nsfx/simulation/i-timer.h>
+#include <nsfx/simulation/timer.h>
+
+
+#endif // SIMULATION_H__3558CCE7_4DE5_4B7F_A9CF_44591D238478
+
