01c2924f0623323649cd95bb875ac41bed584bb6
Wei Tang <gauchyler@uestc.edu.cn>
2018-01-16 11:40:45 +0800

    Added header files for each module.

---
+++ component.h
@@ -0,0 +1,37 @@
+﻿/**
+ * @file
+ *
+ * @brief Component support for Network Simulation Frameworks.
+ *
+ * @version 1.0
+ * @author  Fuzzier Tang <gauchyler@gmail.com>
+ * @date    2018-01-16
+ *
+ * @copyright Copyright (c) 2018.
+ *            National Key Laboratory of Science and Technology on Communications,
+ *            University of Electronic Science and Technology of China.
+ *            All rights reserved.
+ */
+
+#ifndef COMPONENT_H__8C0FC172_57F7_4306_A0F8_16027B30D4A5
+#define COMPONENT_H__8C0FC172_57F7_4306_A0F8_16027B30D4A5
+
+
+#include <nsfx/component/config.h>
+#include <nsfx/component/exception.h>
+#include <nsfx/component/uuid.h>
+
+#include <nsfx/component/i-object.h>
+#include <nsfx/component/ptr.h>
+
+#include <nsfx/component/i-disposable.h>
+
+#include <nsfx/component/i-class-factory.h>
+#include <nsfx/component/class-factory.h>
+
+#include <nsfx/component/i-class-registry.h>
+#include <nsfx/component/class-registry.h>
+
+
+#endif // COMPONENT_H__8C0FC172_57F7_4306_A0F8_16027B30D4A5
+
---
+++ network.h
@@ -0,0 +1,25 @@
+﻿/**
+ * @file
+ *
+ * @brief Network support for Network Simulation Frameworks.
+ *
+ * @version 1.0
+ * @author  Fuzzier Tang <gauchyler@gmail.com>
+ * @date    2018-01-16
+ *
+ * @copyright Copyright (c) 2018.
+ *            National Key Laboratory of Science and Technology on Communications,
+ *            University of Electronic Science and Technology of China.
+ *            All rights reserved.
+ */
+
+#ifndef NETWORK_H__02054161_34C3_440E_9F00_D02DF34B4452
+#define NETWORK_H__02054161_34C3_440E_9F00_D02DF34B4452
+
+
+#include <nsfx/network/config.h>
+#include <nsfx/network/buffer.h>
+
+
+#endif // NETWORK_H__02054161_34C3_440E_9F00_D02DF34B4452
+
