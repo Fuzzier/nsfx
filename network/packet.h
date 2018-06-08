2bc38c083230e2a43449bc20be7ad51761b70e04
Wei Tang <gauchyler@uestc.edu.cn>
2018-06-08 18:11:06 +0800

    Added header file.

---
+++ network/packet.h
@@ -0,0 +1,31 @@
+/**
+ * @file
+ *
+ * @brief Packet for Network Simulation Frameworks.
+ *
+ * @version 1.0
+ * @author  Wei Tang <gauchyler@uestc.edu.cn>
+ * @date    2018-06-08
+ *
+ * @copyright Copyright (c) 2018.
+ *            National Key Laboratory of Science and Technology on Communications,
+ *            University of Electronic Science and Technology of China.
+ *            All rights reserved.
+ */
+
+#ifndef PACKET_H__BC226B52_9613_45DD_ACAF_2B42644DCCEF
+#define PACKET_H__BC226B52_9613_45DD_ACAF_2B42644DCCEF
+
+
+#include <nsfx/network/config.h>
+
+#include <nsfx/network/packet/exception.h>
+#include <nsfx/network/packet/tag.h>
+#include <nsfx/network/packet/tag-index.h>
+#include <nsfx/network/packet/tag-index-array.h>
+#include <nsfx/network/packet/tag-list.h>
+#include <nsfx/network/packet/packet.h>
+
+
+#endif // PACKET_H__BC226B52_9613_45DD_ACAF_2B42644DCCEF
+
