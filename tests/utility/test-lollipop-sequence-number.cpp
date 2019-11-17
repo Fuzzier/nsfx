3376a2fb0ef08c7791c323111447505364fe2e85
Wei Tang <gauchyler@uestc.edu.cn>
2019-11-17 12:37:24 +0800

    Added LollipopSequenceNumber.

---
+++ tests/utility/test-lollipop-sequence-number.cpp
@@ -0,0 +1,442 @@
+/**
+ * @file
+ *
+ * @brief Test LollipopSequenceNumber.
+ *
+ * @version 1.0
+ * @author  Wei Tang <gauchyler@uestc.edu.cn>
+ * @date    date
+ *
+ * @copyright Copyright (c) 2019.
+ *   National Key Laboratory of Science and Technology on Communications,
+ *   University of Electronic Science and Technology of China.
+ *   All rights reserved.
+ */
+
+#include <nsfx/test.h>
+#include <nsfx/utility/lollipop-sequence-number.h>
+#include "nsfx/network/buffer/buffer.h"
+#include <iostream>
+
+
+NSFX_TEST_SUITE(LollipopSequenceNumberTraits)/*{{{*/
+{
+    NSFX_TEST_CASE(1)
+    {
+        typedef nsfx::LollipopSequenceNumber<1>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x1);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0x1);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x0);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint8_t>::value));
+    }
+
+    NSFX_TEST_CASE(2)
+    {
+        typedef nsfx::LollipopSequenceNumber<2>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x2);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0x03);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x00);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint8_t>::value));
+    }
+
+    NSFX_TEST_CASE(8)
+    {
+        typedef nsfx::LollipopSequenceNumber<8>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x80);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE, 0xFF);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE, 0x3F);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint8_t>::value));
+    }
+
+    NSFX_TEST_CASE(9)
+    {
+        typedef nsfx::LollipopSequenceNumber<9>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x0100);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0x01FF);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x007F);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint16_t>::value));
+    }
+
+    NSFX_TEST_CASE(16)
+    {
+        typedef nsfx::LollipopSequenceNumber<16>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x8000);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0xFFFF);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x3FFF);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint16_t>::value));
+    }
+
+    NSFX_TEST_CASE(17)
+    {
+        typedef nsfx::LollipopSequenceNumber<17>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x00010000U);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0x0001FFFFU);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x00007FFFU);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint32_t>::value));
+    }
+
+    NSFX_TEST_CASE(32)
+    {
+        typedef nsfx::LollipopSequenceNumber<32>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x80000000U);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0xFFFFFFFFU);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x3FFFFFFFU);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint32_t>::value));
+    }
+
+    NSFX_TEST_CASE(33)
+    {
+        typedef nsfx::LollipopSequenceNumber<33>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x0000000100000000U);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0x00000001FFFFFFFFU);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x000000007FFFFFFFU);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint64_t>::value));
+    }
+
+    NSFX_TEST_CASE(64)
+    {
+        typedef nsfx::LollipopSequenceNumber<64>::TraitsType TraitsType;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0x8000000000000000U);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0xFFFFFFFFFFFFFFFFU);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x3FFFFFFFFFFFFFFFU);
+        NSFX_TEST_EXPECT((std::is_same<typename TraitsType::ValueType,
+                          nsfx::uint64_t>::value));
+    }
+
+}/*}}}*/
+
+
+NSFX_TEST_SUITE(LollipopSequenceNumber)/*{{{*/
+{
+    NSFX_TEST_CASE(1)
+    {
+        typedef nsfx::LollipopSequenceNumber<1> S;
+        typedef S::TraitsType TraitsType;
+        typedef TraitsType::ValueType V;
+        S s(0);
+        NSFX_TEST_EXPECT_EQ(s, S(0));
+        NSFX_TEST_EXPECT_EQ(s++, S(0));
+        NSFX_TEST_EXPECT_EQ(s, S(1));
+        s = S(1);
+        NSFX_TEST_EXPECT_EQ(++s, S(1));
+        NSFX_TEST_EXPECT_EQ(s, S(1));
+        s = S(0);
+        NSFX_TEST_EXPECT(s == s);
+        NSFX_TEST_EXPECT(!(s != s));
+        NSFX_TEST_EXPECT(s <= s);
+        NSFX_TEST_EXPECT(s >= s);
+        NSFX_TEST_EXPECT(!(s < s));
+        NSFX_TEST_EXPECT(!(s > s));
+        s = S(1);
+        NSFX_TEST_EXPECT(s == s);
+        NSFX_TEST_EXPECT(!(s != s));
+        NSFX_TEST_EXPECT(s <= s);
+        NSFX_TEST_EXPECT(s >= s);
+        NSFX_TEST_EXPECT(!(s < s));
+        NSFX_TEST_EXPECT(!(s > s));
+        NSFX_TEST_EXPECT(S(0) < S(1));
+        NSFX_TEST_EXPECT(S(1) > S(0));
+        NSFX_TEST_EXPECT(!(S(1) < S(0)));
+        NSFX_TEST_EXPECT(!(S(0) > S(1)));
+    }
+
+    NSFX_TEST_CASE(2)
+    {
+        typedef nsfx::LollipopSequenceNumber<2> S;
+        typedef S::TraitsType TraitsType;
+        typedef TraitsType::ValueType V;
+        V F = TraitsType::START_VALUE;
+        V M = TraitsType::MAX_VALUE;
+        V G = TraitsType::GAP_VALUE;
+        S s(0);
+        NSFX_TEST_EXPECT_EQ(s++, S(0));
+        NSFX_TEST_EXPECT_EQ(s, S(1));
+        s = S(1);
+        NSFX_TEST_EXPECT_EQ(++s, S(2));
+        s = S(2);
+        NSFX_TEST_EXPECT_EQ(++s, S(3));
+        s = S(3);
+        NSFX_TEST_EXPECT_EQ(++s, S(2));
+        s = S(0);
+        NSFX_TEST_EXPECT(s == s);
+        NSFX_TEST_EXPECT(!(s != s));
+        NSFX_TEST_EXPECT(s <= s);
+        NSFX_TEST_EXPECT(s >= s);
+        NSFX_TEST_EXPECT(!(s < s));
+        NSFX_TEST_EXPECT(!(s > s));
+        s = S(1);
+        NSFX_TEST_EXPECT(s == s);
+        NSFX_TEST_EXPECT(!(s != s));
+        NSFX_TEST_EXPECT(s <= s);
+        NSFX_TEST_EXPECT(s >= s);
+        NSFX_TEST_EXPECT(!(s < s));
+        NSFX_TEST_EXPECT(!(s > s));
+        //
+        NSFX_TEST_EXPECT(S(0) < S(1));
+        NSFX_TEST_EXPECT(S(0) <= S(1));
+        NSFX_TEST_EXPECT(S(1) > S(0));
+        NSFX_TEST_EXPECT(S(1) >= S(0));
+        //
+        NSFX_TEST_EXPECT(S(1) < S(2));
+        NSFX_TEST_EXPECT(S(1) <= S(2));
+        NSFX_TEST_EXPECT(S(2) > S(1));
+        NSFX_TEST_EXPECT(S(2) >= S(1));
+        //
+        NSFX_TEST_EXPECT(S(2) < S(3));
+        NSFX_TEST_EXPECT(S(2) <= S(3));
+        NSFX_TEST_EXPECT(S(2) > S(3));
+        NSFX_TEST_EXPECT(S(2) >= S(3));
+        //
+        NSFX_TEST_EXPECT(S(3) < S(2));
+        NSFX_TEST_EXPECT(S(3) <= S(2));
+        NSFX_TEST_EXPECT(S(3) > S(2));
+        NSFX_TEST_EXPECT(S(3) >= S(2));
+    }
+
+    NSFX_TEST_CASE(8)
+    {
+        typedef nsfx::LollipopSequenceNumber<8> S;
+        typedef S::TraitsType TraitsType;
+        typedef TraitsType::ValueType V;
+        V F = TraitsType::START_VALUE;
+        V M = TraitsType::MAX_VALUE;
+        V G = TraitsType::GAP_VALUE;
+        S s(M);
+        NSFX_TEST_EXPECT_EQ(s++, S(M));
+        NSFX_TEST_EXPECT_EQ(s, S(F));
+        s = S(M);
+        NSFX_TEST_EXPECT_EQ(++s, S(F));
+        S n(F);
+        s = S(F+G);
+        NSFX_TEST_EXPECT(n != s);
+        NSFX_TEST_EXPECT(n <= s);
+        NSFX_TEST_EXPECT(n < s);
+        NSFX_TEST_EXPECT(s > n);
+        NSFX_TEST_EXPECT(s >= n);
+        ++s;
+        NSFX_TEST_EXPECT(n != s);
+        NSFX_TEST_EXPECT(n >= s);
+        NSFX_TEST_EXPECT(n > s);
+        NSFX_TEST_EXPECT(s < n);
+        NSFX_TEST_EXPECT(s <= n);
+    }
+
+    // bits = 16, start = 1.
+    NSFX_TEST_CASE(16_1)
+    {
+        typedef nsfx::LollipopSequenceNumber<16, 1> S;
+        typedef S::TraitsType TraitsType;
+        typedef TraitsType::ValueType V;
+        V F = TraitsType::START_VALUE;
+        V M = TraitsType::MAX_VALUE;
+        V G = TraitsType::GAP_VALUE;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 1);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0xFFFF);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x7FFF);
+        S s(0);
+        NSFX_TEST_EXPECT_EQ(s++, S(0));
+        NSFX_TEST_EXPECT_EQ(s, S(1));
+        s = S(M);
+        NSFX_TEST_EXPECT_EQ(++s, S(F));
+        //
+        NSFX_TEST_EXPECT(S(0) < S(F));
+        NSFX_TEST_EXPECT(S(0) < S(M));
+        NSFX_TEST_EXPECT(S(F) > S(0));
+        NSFX_TEST_EXPECT(S(M) > S(0));
+        S n(F);
+        s = S(F+G);
+        NSFX_TEST_EXPECT(n != s);
+        NSFX_TEST_EXPECT(n <= s);
+        NSFX_TEST_EXPECT(n < s);
+        NSFX_TEST_EXPECT(s > n);
+        NSFX_TEST_EXPECT(s >= n);
+        ++s;
+        NSFX_TEST_EXPECT(n != s);
+        NSFX_TEST_EXPECT(n >= s);
+        NSFX_TEST_EXPECT(n > s);
+        NSFX_TEST_EXPECT(s < n);
+        NSFX_TEST_EXPECT(s <= n);
+    }
+
+    // bits = 32, start = 0
+    NSFX_TEST_CASE(32_0)
+    {
+        typedef nsfx::LollipopSequenceNumber<32, 0> S;
+        typedef S::TraitsType TraitsType;
+        typedef TraitsType::ValueType V;
+        V F = TraitsType::START_VALUE;
+        V M = TraitsType::MAX_VALUE;
+        V G = TraitsType::GAP_VALUE;
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::START_VALUE, 0);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::MAX_VALUE,   0xFFFFFFFF);
+        NSFX_TEST_EXPECT_EQ((TraitsType::ValueType)TraitsType::GAP_VALUE,   0x7FFFFFFF);
+        S s(0);
+        NSFX_TEST_EXPECT_EQ(s++, S(0));
+        NSFX_TEST_EXPECT_EQ(s, S(1));
+        s = S(M);
+        NSFX_TEST_EXPECT_EQ(++s, S(F));
+        S n(F);
+        s = S(F+G);
+        NSFX_TEST_EXPECT(n != s);
+        NSFX_TEST_EXPECT(n <= s);
+        NSFX_TEST_EXPECT(n < s);
+        NSFX_TEST_EXPECT(s > n);
+        NSFX_TEST_EXPECT(s >= n);
+        ++s;
+        NSFX_TEST_EXPECT(n != s);
+        NSFX_TEST_EXPECT(n >= s);
+        NSFX_TEST_EXPECT(n > s);
+        NSFX_TEST_EXPECT(s < n);
+        NSFX_TEST_EXPECT(s <= n);
+    }
+
+    NSFX_TEST_CASE(64)
+    {
+        typedef nsfx::LollipopSequenceNumber<64> S;
+        typedef S::TraitsType TraitsType;
+        typedef TraitsType::ValueType V;
+        V F = TraitsType::START_VALUE;
+        V M = TraitsType::MAX_VALUE;
+        V G = TraitsType::GAP_VALUE;
+        S s(M);
+        NSFX_TEST_EXPECT_EQ(s++, S(M));
+        NSFX_TEST_EXPECT_EQ(s, S(F));
+        s = S(M);
+        NSFX_TEST_EXPECT_EQ(++s, S(F));
+        S n(F);
+        s = S(F+G);
+        NSFX_TEST_EXPECT(n != s);
+        NSFX_TEST_EXPECT(n <= s);
+        NSFX_TEST_EXPECT(n < s);
+        NSFX_TEST_EXPECT(s > n);
+        NSFX_TEST_EXPECT(s >= n);
+        ++s;
+        NSFX_TEST_EXPECT(n != s);
+        NSFX_TEST_EXPECT(n >= s);
+        NSFX_TEST_EXPECT(n > s);
+        NSFX_TEST_EXPECT(s < n);
+        NSFX_TEST_EXPECT(s <= n);
+    }
+
+    NSFX_TEST_CASE(Swap)
+    {
+        typedef nsfx::LollipopSequenceNumber<23> S;
+        S s0(0x12345);
+        S s1(0x6789a);
+        swap(s0, s1);
+        NSFX_TEST_EXPECT_EQ(s0, S(0x6789a));
+        NSFX_TEST_EXPECT_EQ(s1, S(0x12345));
+    }
+
+    NSFX_TEST_CASE(Hash)
+    {
+        typedef nsfx::LollipopSequenceNumber<23> S;
+        S s(0x12345);
+        size_t h = boost::hash<S>()(s);
+    }
+
+    NSFX_TEST_CASE(IO32)
+    {
+        typedef nsfx::LollipopSequenceNumber<23> S;
+        S s0(0x12345);
+        S s1;
+        nsfx::Buffer buffer;
+        NSFX_TEST_EXPECT_EQ(S::GetSize(), 3);
+        buffer.AddAtStart(S::GetSize());
+        auto it = buffer.begin();
+        ////////////////////
+        s0.Write(it);
+        it -= 3;
+        s1.Read(it);
+        it -= 3;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x12345));
+        s0.WriteL(it);
+        it -= 3;
+        s1.ReadL(it);
+        it -= 3;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x12345));
+        s0.WriteB(it);
+        it -= 3;
+        s1.ReadB(it);
+        it -= 3;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x12345));
+        ////////////////////
+        Write(it, s0);
+        it -= 3;
+        Read(it, &s1);
+        it -= 3;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x12345));
+        WriteL(it, s0);
+        it -= 3;
+        ReadL(it, &s1);
+        it -= 3;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x12345));
+        WriteB(it, s0);
+        it -= 3;
+        ReadB(it, &s1);
+        it -= 3;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x12345));
+    }
+
+    NSFX_TEST_CASE(IO64)
+    {
+        typedef nsfx::LollipopSequenceNumber<55> S;
+        S s0(0x123456789abcdef);
+        S s1;
+        nsfx::Buffer buffer;
+        NSFX_TEST_EXPECT_EQ(S::GetSize(), 7);
+        buffer.AddAtStart(S::GetSize());
+        auto it = buffer.begin();
+        ////////////////////
+        s0.Write(it);
+        it -= 7;
+        s1.Read(it);
+        it -= 7;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x123456789abcdef));
+        s0.WriteL(it);
+        it -= 7;
+        s1.ReadL(it);
+        it -= 7;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x123456789abcdef));
+        s0.WriteB(it);
+        it -= 7;
+        s1.ReadB(it);
+        it -= 7;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x123456789abcdef));
+        ////////////////////
+        Write(it, s0);
+        it -= 7;
+        Read(it, &s1);
+        it -= 7;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x123456789abcdef));
+        WriteL(it, s0);
+        it -= 7;
+        ReadL(it, &s1);
+        it -= 7;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x123456789abcdef));
+        WriteB(it, s0);
+        it -= 7;
+        ReadB(it, &s1);
+        it -= 7;
+        NSFX_TEST_EXPECT_EQ(s0, S(0x123456789abcdef));
+    }
+
+}/*}}}*/
+
+
+int main(void)
+{
+    nsfx::test::runner::GetLogger()->AddStreamSink(std::cerr);
+    nsfx::test::runner::Run();
+
+    return 0;
+}
+
---
+++ utility/lollipop-sequence-number.h
@@ -0,0 +1,402 @@
+﻿/**
+ * @file
+ *
+ * @brief Utility for Network Simulation Frameworks.
+ *
+ * @version 1.0
+ * @author  Wei Tang <gauchyler@uestc.edu.cn>
+ * @date    2019-11-16
+ *
+ * @copyright Copyright (c) 2019.
+ *            National Key Laboratory of Science and Technology on Communications,
+ *            University of Electronic Science and Technology of China.
+ *            All rights reserved.
+ */
+
+#ifndef LOLLIPOP_SEQUENCE_NUMBER_H__092CC9EE_D2A1_4037_AFB4_6E950BFDA676
+#define LOLLIPOP_SEQUENCE_NUMBER_H__092CC9EE_D2A1_4037_AFB4_6E950BFDA676
+
+
+#include <nsfx/utility/config.h>
+#include <boost/integer.hpp>
+#include <boost/functional/hash.hpp>
+#include <boost/core/swap.hpp>
+#include <type_traits> // is_unsigned, is_integral, make_unsigned, conditional
+#include <nsfx/utility/endian.h>
+#include <nsfx/network/buffer/iterator/basic-buffer-iterator.h>
+#include <iostream>
+
+
+NSFX_OPEN_NAMESPACE
+
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * @ingroup Utility
+ * @brief The traits of lollipop sequence number.
+ *
+ * Users **shall not** use this class directly.
+ * Instead, use `LollipopSequenceNumber::TraitsType` to access the traits.
+ */
+template<size_t bits, typename boost::uint_t<bits>::least start>
+class LollipopSequenceNumberTraits/*{{{*/
+{
+public:
+    typedef typename boost::uint_t<bits>::least  ValueType;
+
+    // Maximum number of the circular part, `2^bits - 1`.
+    BOOST_STATIC_CONSTANT(ValueType, MAX_VALUE =
+        static_cast<ValueType>(-1) >> (sizeof (ValueType) * 8 - bits));
+
+    // First number of the circular part.
+    BOOST_STATIC_CONSTANT(ValueType, START_VALUE =
+                          static_cast<ValueType>(start));
+
+    // Gap of the circular part, no more than half of the circular number space.
+    BOOST_STATIC_CONSTANT(ValueType, GAP_VALUE = (MAX_VALUE - start) / 2);
+
+};/*}}}*/
+
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * @ingroup Utility
+ * @brief A lollipop sequence number.
+ *
+ * @tparam bits The number of bits. Must be within `[1, 64]`.
+ * @tparam start The first number of the circular part.
+ *               It **must** be within `[0, N-1]`, where `N = 2^bits`.
+ *
+ * The range of lollipop sequence number is `[0, N-1]`.
+ * The linear part is `[0, start-1]`, and the circular part is `[start, N-1]`.
+ *
+ * The default value of `start` is `N/2 = 2^(bits-1)`.
+ *
+ * A lollipop sequence number is represented as an *unsigned* integral number.
+ */
+template<size_t bits, typename boost::uint_t<bits>::least start =
+        (static_cast<typename boost::uint_t<bits>::least>(-1) >>
+           (sizeof (typename boost::uint_t<bits>::least)*8-bits))/2 + 1>
+class LollipopSequenceNumber/*{{{*/
+{
+public:
+    typedef LollipopSequenceNumber                    ThisType;
+    typedef LollipopSequenceNumberTraits<bits, start> TraitsType;
+    typedef typename TraitsType::ValueType            ValueType;
+
+public:
+    LollipopSequenceNumber(void) BOOST_NOEXCEPT :
+        value_(0)
+    {
+    }
+
+    explicit LollipopSequenceNumber(ValueType value) :
+        value_(value <= TraitsType::MAX_VALUE ? value : TraitsType::MAX_VALUE)
+    {
+    }
+
+    ValueType GetValue(void) const BOOST_NOEXCEPT
+    {
+        return value_;
+    }
+
+    /**
+     * @brief The number bytes that is just enough to hold `bits`.
+     *
+     * Buffer I/O will read/write just such number of bytes.
+     */
+    static size_t GetSize(void) BOOST_NOEXCEPT
+    {
+        typedef bits_endian_traits<ValueType, bits>  B;
+        return B::size;
+    }
+
+    static size_t GetBitSize(void) BOOST_NOEXCEPT
+    {
+        return bits;
+    }
+
+    // Increment.
+private:
+    void InternalIncrement(void) BOOST_NOEXCEPT
+    {
+        if (value_ == TraitsType::MAX_VALUE)
+        {
+            value_ = TraitsType::START_VALUE;
+        }
+        else
+        {
+            ++value_;
+        }
+    }
+
+public:
+    ThisType& operator++(void) BOOST_NOEXCEPT
+    {
+        InternalIncrement();
+        return *this;
+    }
+
+    ThisType operator++(int) BOOST_NOEXCEPT
+    {
+        ValueType old = value_;
+        InternalIncrement();
+        return ThisType(old);
+    }
+
+    // Comparison.
+private:
+    // Space of circular part.
+    struct OneNumberTag  {};
+    struct TwoNumbersTag   {};
+    struct MoreNumbersTag {};
+    template<ValueType space> struct SpaceTag : MoreNumbersTag {};
+    template<> struct SpaceTag<1> : TwoNumbersTag {};
+    template<> struct SpaceTag<0> : OneNumberTag {};
+    typedef SpaceTag<TraitsType::MAX_VALUE - TraitsType::START_VALUE>
+            MySpaceTag;
+
+    // The circular part has only one number, that is `start`.
+    static bool InternalLessThan(ValueType lhs, ValueType rhs, OneNumberTag) BOOST_NOEXCEPT
+    {
+        return lhs < rhs;
+    }
+
+    static bool InternalLessEqual(ValueType lhs, ValueType rhs, OneNumberTag) BOOST_NOEXCEPT
+    {
+        return lhs <= rhs;
+    }
+
+    // The circular part has only two numbers, that is `start` and `start+1`.
+    static bool InternalLessThan(ValueType lhs, ValueType rhs, TwoNumbersTag) BOOST_NOEXCEPT
+    {
+        bool result = true;
+        if (rhs < TraitsType::START_VALUE)
+        {
+            result = lhs < rhs;
+        }
+        return result;
+    }
+
+    static bool InternalLessEqual(ValueType lhs, ValueType rhs, TwoNumbersTag) BOOST_NOEXCEPT
+    {
+        bool result = true;
+        if (rhs < TraitsType::START_VALUE)
+        {
+            result = lhs <= rhs;
+        }
+        return result;
+    }
+
+    // The circular part has more than two numbers.
+    static bool InternalLessThan(ValueType lhs, ValueType rhs, MoreNumbersTag) BOOST_NOEXCEPT
+    {
+        bool result = false;
+        if (lhs < TraitsType::START_VALUE)
+        {
+            result = lhs < rhs;
+        }
+        else if (lhs < rhs)
+        {
+            result = (rhs - lhs) <= TraitsType::GAP_VALUE;
+        }
+        else if (rhs >= TraitsType::START_VALUE)
+        {
+            result = (lhs - rhs) > TraitsType::GAP_VALUE;
+        }
+        return result;
+    }
+
+    static bool InternalLessEqual(ValueType lhs, ValueType rhs, MoreNumbersTag) BOOST_NOEXCEPT
+    {
+        bool result = false;
+        if (lhs < TraitsType::START_VALUE)
+        {
+            result = lhs <= rhs;
+        }
+        else if (lhs <= rhs)
+        {
+            result = (rhs - lhs) <= TraitsType::GAP_VALUE;
+        }
+        else if (rhs >= TraitsType::START_VALUE)
+        {
+            result = (lhs - rhs) > TraitsType::GAP_VALUE;
+        }
+        return result;
+    }
+
+public:
+    bool operator< (const ThisType& rhs) const BOOST_NOEXCEPT
+    {
+        return InternalLessThan(value_, rhs.value_, MySpaceTag());
+    }
+
+    bool operator<=(const ThisType& rhs) const BOOST_NOEXCEPT
+    {
+        return InternalLessEqual(value_, rhs.value_, MySpaceTag());
+    }
+
+    bool operator==(const ThisType& rhs) const BOOST_NOEXCEPT
+    {
+        return value_ == rhs.value_;
+    }
+
+    bool operator!=(const ThisType& rhs) const BOOST_NOEXCEPT
+    {
+        return value_ != rhs.value_;
+    }
+
+    bool operator> (const ThisType& rhs) const BOOST_NOEXCEPT
+    {
+        return InternalLessThan(rhs.value_, value_, MySpaceTag());
+    }
+
+    bool operator>=(const ThisType& rhs) const BOOST_NOEXCEPT
+    {
+        return InternalLessEqual(rhs.value_, value_, MySpaceTag());
+    }
+
+    void swap(ThisType& rhs) BOOST_NOEXCEPT
+    {
+        if (this != &rhs)
+        {
+            boost::swap(value_, rhs.value_);
+        }
+    }
+
+    ////////////////////////////////////////
+    // Buffer I/O.
+public:
+    template<bool zcAware>
+    void Write(BasicBufferIterator</*readOnly*/false, zcAware>& it) const
+    {
+        typedef bits_endian_traits<ValueType, bits>  B;
+        it.Write(bytes_ + B::offset, B::size);
+    }
+
+    template<bool zcAware>
+    void WriteL(BasicBufferIterator</*readOnly*/false, zcAware>& it) const
+    {
+        typedef bits_endian_traits<ValueType, bits>  B;
+        it.WriteL(bytes_ + B::offset, B::size);
+    }
+
+    template<bool zcAware>
+    void WriteB(BasicBufferIterator</*readOnly*/false, zcAware>& it) const
+    {
+        typedef bits_endian_traits<ValueType, bits>  B;
+        it.WriteB(bytes_ + B::offset, B::size);
+    }
+
+    template<bool readOnly, bool zcAware>
+    void Read(BasicBufferIterator<readOnly, zcAware>& it)
+    {
+        typedef bits_endian_traits<ValueType, bits>  B;
+        it.Read(bytes_ + B::offset, B::size);
+    }
+
+    template<bool readOnly, bool zcAware>
+    void ReadL(BasicBufferIterator<readOnly, zcAware>& it)
+    {
+        typedef bits_endian_traits<ValueType, bits>  B;
+        it.ReadL(bytes_ + B::offset, B::size);
+    }
+
+    template<bool readOnly, bool zcAware>
+    void ReadB(BasicBufferIterator<readOnly, zcAware>& it)
+    {
+        typedef bits_endian_traits<ValueType, bits>  B;
+        it.ReadB(bytes_ + B::offset, B::size);
+    }
+
+private:
+    union
+    {
+        ValueType value_;
+        uint8_t   bytes_[sizeof (ValueType)];
+    };
+
+};/*}}}*/
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Free functions./*{{{*/
+template<size_t bits, typename boost::uint_t<bits>::least start>
+inline size_t
+hash_value(const LollipopSequenceNumber<bits, start>& sn) BOOST_NOEXCEPT
+{
+    typedef typename LollipopSequenceNumber<bits, start>::ValueType  ValueType;
+    return boost::hash<ValueType>()(sn.GetValue());
+}
+
+template<size_t bits, typename boost::uint_t<bits>::least start>
+inline void
+swap(LollipopSequenceNumber<bits, start>& lhs,
+     LollipopSequenceNumber<bits, start>& rhs) BOOST_NOEXCEPT
+{
+    return lhs.swap(rhs);
+}
+
+template<class Char, class Traits, size_t bits, typename boost::uint_t<bits>::least start>
+inline std::basic_ostream<Char, Traits>&
+operator<<(std::basic_ostream<Char, Traits>& os,
+           const LollipopSequenceNumber<bits, start>& sn)
+{
+    typedef LollipopSequenceNumber<bits, start>::ValueType  ValueType;
+    typedef std::conditional<sizeof (ValueType) <= sizeof (unsigned int),
+                             unsigned int, ValueType>::type  U;
+    return os << static_cast<U>(sn.GetValue());
+}
+
+////////////////////////////////////////
+// Buffer I/O.
+template<bool zcAware, size_t bits, typename boost::uint_t<bits>::least start>
+void Write(BasicBufferIterator</*readOnly*/false, zcAware>& it,
+           const LollipopSequenceNumber<bits, start>& sn)
+{
+    sn.Write(it);
+}
+
+template<bool zcAware, size_t bits, typename boost::uint_t<bits>::least start>
+void WriteL(BasicBufferIterator</*readOnly*/false, zcAware>& it,
+           const LollipopSequenceNumber<bits, start>& sn)
+{
+    sn.WriteL(it);
+}
+
+template<bool zcAware, size_t bits, typename boost::uint_t<bits>::least start>
+void WriteB(BasicBufferIterator</*readOnly*/false, zcAware>& it,
+           const LollipopSequenceNumber<bits, start>& sn)
+{
+    sn.WriteB(it);
+}
+
+template<bool readOnly, bool zcAware, size_t bits, typename boost::uint_t<bits>::least start>
+void Read(BasicBufferIterator<readOnly, zcAware>& it,
+           LollipopSequenceNumber<bits, start>* sn)
+{
+    sn->Read(it);
+}
+
+template<bool readOnly, bool zcAware, size_t bits, typename boost::uint_t<bits>::least start>
+void ReadL(BasicBufferIterator<readOnly, zcAware>& it,
+           LollipopSequenceNumber<bits, start>* sn)
+{
+    sn->ReadL(it);
+}
+
+template<bool readOnly, bool zcAware, size_t bits, typename boost::uint_t<bits>::least start>
+void ReadB(BasicBufferIterator<readOnly, zcAware>& it,
+           LollipopSequenceNumber<bits, start>* sn)
+{
+    sn->ReadB(it);
+}
+
+/*}}}*/
+
+
+NSFX_CLOSE_NAMESPACE
+
+
+#endif // LOLLIPOP_SEQUENCE_NUMBER_H__092CC9EE_D2A1_4037_AFB4_6E950BFDA676
+
